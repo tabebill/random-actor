df16fb56cdfa86dc508a6fac6ca3ca32e25492b6
Bill <tabebill09@gmail.com>
2023-10-16 21:25:22 +0100

    creation of terraform state files

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+# Define default AWS region
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+# Define key pair name
+variable "key_name" {
+    type = string
+    default = "my_keypair"
+}
+
+# Define the GitHub repository URL
+variable "github_repo_url" {
+  type    = string
+  default = "https://github.com/tabebill/random-actor.git"
+}
+
+# Define the name of your Docker image repository in ECR
+variable "ecr_repository_name" {
+  type    = string
+  default = "my-docker-repo"
+}
+
+# Define the CodePipeline artifact bucket name
+variable "artifact_bucket_name" {
+  type    = string
+  default = "my-codepipeline-artifacts"
+}
+
